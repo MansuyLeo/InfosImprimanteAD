548b0b2c77840c60e0e721bfb6a7ef183cfe3bf1
Mansuy Léo <169048834+MansuyLeo@users.noreply.github.com>
2025-01-31 18:55:59 +0100

    Update Imprimantes.bat

--- Imprimantes.bat
+++ Imprimantes.bat
@@ -3,7 +3,7 @@
 set scriptDir=%~dp0
 
 :: Lancement du script Imprimantes.ps1
-powershell.exe -NoProfile -ExecutionPolicy Bypass -File "%scriptDir%Imprimantes.ps1"
+powershell.exe -NoProfile -NoLogo -ExecutionPolicy Bypass -File "%scriptDir%Imprimantes.ps1"
 
 :: Pause pour laisser la fenêtre ouverte après l'execution
 pause
