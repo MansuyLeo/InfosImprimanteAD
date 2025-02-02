d6623ebd65b0dbe46b0b305faba692a8d8bd62ec
Mansuy Léo <169048834+MansuyLeo@users.noreply.github.com>
2025-02-02 21:46:30 +0100

    Update Imprimantes.bat

--- Imprimantes.bat
+++ Imprimantes.bat
@@ -1,4 +1,17 @@
+:: Script par MANSUY Léo - Alternant TAM PSL
+
 @echo off
+
+echo.
+echo ##############################################
+echo #                                            #
+echo #            INFOS IMPRIMANTES AD            #
+echo #   Auteur: MANSUY Leo - Alternant TAM PSL   #
+echo #              e.SNCF Solutions              #
+echo #                                            #
+echo ##############################################
+echo.
+:loop
 :: Dossier actuel du script .bat
 set scriptDir=%~dp0
 
@@ -6,4 +19,12 @@ set scriptDir=%~dp0
 powershell.exe -NoProfile -NoLogo -ExecutionPolicy Bypass -File "%scriptDir%Imprimantes.ps1"
 
 :: Pause pour laisser la fenêtre ouverte après l'execution
-pause
+echo.
+:prompt
+set /p choice="Voulez-vous rechercher une autre imprimante ? (y/n): "
+if /i "%choice%"=="y" goto loop
+if /i "%choice%"=="n" exit
+
+:: Message d'erreur pour une entrée non valide
+echo Choix invalide. Veuillez entrer 'y' ou 'n'.
+goto prompt
